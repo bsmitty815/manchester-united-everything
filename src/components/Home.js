c5afef142917bf89ba47d0fbf6b1a0d364386383
Brian Smith <brian.bubbam@gmail.com>
2021-07-23 19:09:59 -0500

    updated home image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import teamImage from "../teamImage.jpeg"
 
 
 
@@ -9,7 +9,7 @@ function Home() {
     return (
         <div >
             <div className="home" >
-                <img src="https://images.daznservices.com/di/library/GOAL/51/e/rashford-greenwood-manchester-united-2021_fzb7eveoqdhl1u6mdq16yw2i5.jpg?quality=60&w=1000" />
+                <img src={teamImage} />
             </div>
             <div className="home" >
                 <iframe title="manchester united" src="https://giphy.com/embed/SWhLaOdoGhrQ7GAfOk" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
