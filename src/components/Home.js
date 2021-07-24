16f15a0b6d840dce44b46541317f3e8a364320d6
Brian Smith <brian.bubbam@gmail.com>
2021-07-23 19:02:11 -0500

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,15 @@ function Home() {
 
 
     return (
-        <div className="home" >
-            <iframe title="manchester united" src="https://giphy.com/embed/SWhLaOdoGhrQ7GAfOk" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+        <div >
+            <div className="home" >
+                <img src="https://images.daznservices.com/di/library/GOAL/51/e/rashford-greenwood-manchester-united-2021_fzb7eveoqdhl1u6mdq16yw2i5.jpg?quality=60&w=1000" />
+            </div>
+            <div className="home" >
+                <iframe title="manchester united" src="https://giphy.com/embed/SWhLaOdoGhrQ7GAfOk" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            </div>
+            
+            
         </div>
     )
 
